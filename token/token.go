8a89a7944ae9d474d4f1dd90b4a9fa8f3e4a0de2
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-03 13:11:20 +0900

    create toke file

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//識別子+リテラル
+	INDENT = "INDENT" //add,foobar,s,y,...
+	INT    = "INT"    //1343456
+
+	//デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	キーワード
+	FUNCTIOIN = "FUNCTION"
+	LET       = "LET"
+)
