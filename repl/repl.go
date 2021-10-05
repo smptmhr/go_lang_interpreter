990e7820cb450467653b45f159feadeb4f4a3225
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-05 12:33:10 +0900

    fix typo in repl.go lin:25

--- repl/repl.go
+++ repl/repl.go
@@ -22,7 +22,7 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 
-		for tok := l.NexetToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
 	}
