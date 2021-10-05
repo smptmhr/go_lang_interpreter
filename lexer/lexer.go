61e3e574819842c767dce1c11a6fd775264bba14
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-05 11:10:46 +0900

    create peekChar()

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,6 +25,14 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
