cb14b857929ff38b95bd986f91ff4995e76fc0a4
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-10 18:00:07 +0900

    create test for parsing infix expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -198,6 +198,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 		}
 	}
 }
+
 func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 	integ, ok := il.(*ast.IntegerLiteral)
 	if !ok {
