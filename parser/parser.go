a931b78c1fdb31cc4f582f40530cdab92e7e4b6e
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-25 14:36:54 +0900

    create function to parse parameter of FunctionLiteral

--- parser/parser.go
+++ parser/parser.go
@@ -95,6 +95,32 @@ func (p *Parser) parseFunctionLiteral() ast.Expression {
 	return lit
 }
 
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifiers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) { //パラメータが無いとき
+		p.nextToken()
+		return identifiers
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+	identifiers = append(identifiers, ident)
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken() //currentToken : COMMA
+		p.nextToken() //currentTOken : Identifier
+		ident := &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	return identifiers
+}
+
 func (p *Parser) parseIfExpression() ast.Expression {
 	expression := &ast.IfExpression{Token: p.currentToken}
 	if !p.expectPeek(token.LPAREN) {
