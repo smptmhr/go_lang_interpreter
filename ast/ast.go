89805903ba5d35454a6c8e3370252a172d9c328d
smptmhr <tomohiro.shimpo@gmail.com>
2021-10-25 13:58:11 +0900

    correct typing miss

--- ast/ast.go
+++ ast/ast.go
@@ -210,7 +210,7 @@ type FunctionLiteral struct {
 }
 
 func (fl *FunctionLiteral) expressionNode()      {}
-func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.LiteralLiteral }
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
 func (fl *FunctionLiteral) String() string {
 	var out bytes.Buffer
 
