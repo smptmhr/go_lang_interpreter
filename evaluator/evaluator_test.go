b89135fea0c6a1de327c00c9baf9a938cc19be9b
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-08 14:29:28 +0900

    fix typo

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -25,7 +25,7 @@ func TestEvalIntegerExpression(t *testing.T) {
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
-	program := p.ParserProgram()
+	program := p.ParseProgram()
 
 	return Eval(program)
 }
