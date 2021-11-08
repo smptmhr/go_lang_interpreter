c3bd264fe1d1418579bbf939fe599b4188192fec
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-08 13:52:02 +0900

    fix script to readable

--- object/object.go
+++ object/object.go
@@ -18,8 +18,8 @@ type Integer struct {
 	Value int64
 }
 
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
-func (o *Integer) Type() ObjectType { return INTEGER_OBJ }
 
 type Boolean struct {
 	Value bool
