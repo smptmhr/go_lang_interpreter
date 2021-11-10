913daa2f797689863ab4d0a5fb128e3f8d3f0cc4
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-10 21:04:11 +0900

    add test for infix expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -64,6 +64,14 @@ func TestEvalBoolExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
