65454848a5e854470092acb6519ae9b4defb32ce
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-13 13:51:33 +0900

    rename 'evalStatemetns' to 'evalProgram' to limit useage

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,7 +16,7 @@ func Eval(node ast.Node) object.Object {
 
 	//文
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -131,10 +131,10 @@ func evalIntegerInfixExpression(
 	}
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
 
 		if returnValue, ok := result.(*object.ReturnValue); ok {
