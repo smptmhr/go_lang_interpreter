e0b440a980aede471a146270e9b5c9a4524a67d9
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-22 15:28:18 +0900

    create test for binding variavles

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -184,6 +184,10 @@ func TestErrorHandling(t *testing.T) {
 		expectedMessage string
 	}{
 		{
+			"foobar",
+			"identifier not found: foobar",
+		},
+		{
 			"5 + true;",
 			"type mismatch: INTEGER + BOOLEAN",
 		},
@@ -231,5 +235,20 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5* 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a;let c = a + b + 5 c;", 15},
+	}
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
