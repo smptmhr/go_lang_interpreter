9d6eb0a63956a67a7592bb73a69842cdf6fb60f8
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-23 01:13:08 +0900

    fix typo

--- object/environment.go
+++ object/environment.go
@@ -2,7 +2,7 @@ package object
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 type Environment struct {
@@ -12,8 +12,8 @@ type Environment struct {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
-	if !ok && e.outer !=nil{
-		obj,ok = e.outer.Get(name)
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
 	}
 	return obj, ok
 }
@@ -23,8 +23,8 @@ func (e *Environment) Set(name string, val Object) Object {
 	return val
 }
 
-func NewEnclosedEnvironment(outer *Environment) *object.Environment {
+func NewEnclosedEnvironment(outer *Environment) *Environment {
 	env := NewEnvironment()
-	env.outer := outer
+	env.outer = outer
 	return env
 }
