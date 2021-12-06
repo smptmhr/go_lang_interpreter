6f9579c503f624121c676b9a2b6a7bf67f6f23cd
smptmhr <tomohiro.shimpo@gmail.com>
2021-12-06 13:18:53 +0900

    when main.go has args(sourcefile name), print contents of them

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"monkey/repl"
 	"os"
@@ -8,11 +10,33 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	filename := flag.Args()
+	switch len(filename) {
+	case 0:
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("hello %s! This is the Monkey programming language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+
+	case 1:
+		fp, err := os.Open(filename[0])
+		if err != nil {
+			panic(err)
+		}
+
+		scanner := bufio.NewScanner(fp)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+		fp.Close()
+
+	default:
+		fmt.Printf("Argument length must be 1. got=%d\n", len(filename))
+		return
 	}
-	fmt.Printf("hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
 }
